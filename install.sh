fb4eb7a3fbae0d96a3783205fa524730689e8798
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2017-09-10 12:46:13 +0200

    Removed sudo from scripts

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ fi
 
 if [ -f 'bf2java' ]; then
     if [ -d '/usr/local/bin/' ]; then
-        sudo cp 'bf2java' '/usr/local/bin/'
+        cp 'bf2java' '/usr/local/bin/'
     fi
 fi
 
 if [ -d '/usr/local/share/man/man1/' ]; then
-    sudo cp 'bf2java.1' '/usr/local/share/man/man1/'
-    sudo mandb -q
+    cp 'bf2java.1' '/usr/local/share/man/man1/'
+    mandb -q
 fi
