d5a819bbeff6872efad9478844ab88464c7ce664
Urs-Björn Schmidt <ursbjoernschmidt@googlemail.com>
2018-12-02 12:56:20 +0100

    Re-added example.sh

---
+++ example.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+befunge="$1"
+classname='Main'
+jasmin="${classname}.jasmin"
+class="${classname}.class"
+
+if [ ! -f 'bf2java' ]; then
+    ./build.sh
+fi
+
+if [ -f "$befunge" ]; then
+    ./bf2java "$befunge" > "$jasmin"
+    if [ -f "$jasmin" ]; then
+        assemble "$jasmin" >/dev/null
+        if [ -f "$class" ]; then
+            java -noverify "$classname"
+        else
+            echo "File not found $class"
+        fi
+    else
+        echo "File not found $jasmin"
+    fi
+else
+    echo "File not found $befunge"
+fi
+
+if [ -f "$class" ]; then
+    rm "$class"
+fi
+
+if [ -f "$jasmin" ]; then
+    rm "$jasmin"
+fi
